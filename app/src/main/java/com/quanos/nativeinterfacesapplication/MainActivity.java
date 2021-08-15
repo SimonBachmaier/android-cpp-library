460239f6433a55458fb7e671887adf19e69fb997
SimonBachmaier <simon.bachmaier@freenet.de>
2021-08-15 09:34:09 +0200

    update test output

--- app/src/main/java/com/quanos/nativeinterfacesapplication/MainActivity.java
+++ app/src/main/java/com/quanos/nativeinterfacesapplication/MainActivity.java
@@ -22,55 +22,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
+        TextView helloMessage = findViewById(R.id.sample_text);
         NativeFunctions nativeFunctions = NativeFunctions.getInstance();
-
-        tv.setText(nativeFunctions.HelloMessage("Java"));
+        helloMessage.setText(nativeFunctions.HelloMessage("Java"));
 
         Button showArticlesButton = findViewById(R.id.showArticlesButton);
-        showArticlesButton.setOnClickListener(v -> {
-            // Run test
-            long t1 = new Date().getTime();
-            int testNumber = 0;
-            for (int i = 0; i < 10000000; i++) {
-                testNumber = nativeFunctions.AddOne(testNumber);
-            }
-            long t2 = new Date().getTime();
-            tv.setText("Test took " + (t2-t1) + "ms");
+        showArticlesButton.setOnClickListener(v -> ShowArticlesAndRunTest(helloMessage, nativeFunctions));
+    }
 
-            /**
-             * Connect to database and read data
-             */
-            System.out.println("--------------------------------------------------------------");
-            System.out.println(getFilesDir().getAbsolutePath() + "/android_test.db");
-            System.out.println(nativeFunctions.openDatabaseConnection(getFilesDir().getAbsolutePath() + "/android_test.db"));
-            nativeFunctions.setupTestData();
-            User[] users = nativeFunctions.getAllUsers();
-            Article[] articles = nativeFunctions.getAllArticles();
+    private void ShowArticlesAndRunTest(TextView tv, NativeFunctions nativeFunctions) {
+        // Run function call performance test
+        int iterations = 10000000;
+        int testNumber = 0;
+        long t1, t2;
 
-            StringBuilder output = new StringBuilder();
-            for (Article article: articles) {
-                for (User user: users) {
-                    if (article.authorId == user.id) {
-                        output.append("----- Article -----\n");
-                        output.append("id: " + article.id + "\n");
-                        output.append("headline: " + article.headline + "\n");
-                        output.append("content: " + article.content + "\n");
-                        output.append("authorId: " + article.authorId + "\n");
-                        output.append("authorName: " + user.name + "\n");
-                    }
+        t1 = new Date().getTime();
+        for (int i = 0; i < iterations; i++) {
+            testNumber = nativeFunctions.AddOne(testNumber);
+        }
+        t2 = new Date().getTime();
+        long cppTime = t2 - t1;
+
+        testNumber = 0;
+        t1 = new Date().getTime();
+        for (int i = 0; i < iterations; i++) {
+            testNumber = AddOne(testNumber);
+        }
+        t2 = new Date().getTime();
+        long javaTime = t2 - t1;
+
+        long timeDiff = cppTime - javaTime;
+        tv.setText("C++ test took " + cppTime + "ms. Java test took " + javaTime + "ms. C++ was " + timeDiff + "ms " + (timeDiff > 0 ? "slower." : "faster."));
+
+        /**
+         * Connect to database and read data
+         */
+        System.out.println("--------------------------------------------------------------");
+        System.out.println(getFilesDir().getAbsolutePath() + "/android_test.db");
+        System.out.println(nativeFunctions.openDatabaseConnection(getFilesDir().getAbsolutePath() + "/android_test.db"));
+        nativeFunctions.setupTestData();
+        User[] users = nativeFunctions.getAllUsers();
+        Article[] articles = nativeFunctions.getAllArticles();
+
+        StringBuilder output = new StringBuilder();
+        for (Article article: articles) {
+            for (User user: users) {
+                if (article.authorId == user.id) {
+                    output.append("----- Article -----\n");
+                    output.append("id: " + article.id + "\n");
+                    output.append("headline: " + article.headline + "\n");
+                    output.append("content: " + article.content + "\n");
+                    output.append("authorId: " + article.authorId + "\n");
+                    output.append("authorName: " + user.name + "\n");
                 }
             }
-            output.append("-------------------");
-            System.out.println(output);
-            System.out.println(nativeFunctions.closeDatabaseConnection());
+        }
+        output.append("-------------------");
+        System.out.println(output);
+        System.out.println(nativeFunctions.closeDatabaseConnection());
 
-            TextView articlesTextView = findViewById(R.id.articlesTextView);
-            articlesTextView.setMovementMethod(new ScrollingMovementMethod());
-            articlesTextView.setText(output);
-        });
+        TextView articlesTextView = findViewById(R.id.articlesTextView);
+        articlesTextView.setMovementMethod(new ScrollingMovementMethod());
+        articlesTextView.setText(output);
+    }
+
+    private int AddOne(int x) {
+        return x + 1;
     }
 }
